e36c68b5ea6d39398350acc86a600fc1b6767ebc
Yutiy Kulikov <kulikov-y@yandex.ru>
2025-04-22 22:02:42 +0300

    загрузка товаров работает

--- js/getGoods.js
+++ js/getGoods.js
@@ -3,7 +3,37 @@ const getGods = () => {
 
   const renderGoods = (goods) => {
     const goodsContainer = document.querySelector(".long-goods-list");
-    console.log(goodsContainer);
+
+    goodsContainer.innerHTML = "";
+
+    goods.forEach((good) => {
+      const goodBlock = document.createElement("div");
+      goodBlock.classList.add("col-lg-3");
+      goodBlock.classList.add("col-sm-6");
+
+      goodBlock.innerHTML = `
+          <div class="goods-card">
+              <span class="label ${good.label ? null : "d-none"}">${
+        good.label
+      }</span>
+              <img src="db/${good.img}" alt="${good.name}"
+                class="goods-image">
+              <h3 class="goods-title">${good.name}</h3>
+              
+              <p class="goods-description">${good.description}</p>
+             
+              <button class="button goods-card-btn add-to-cart" data-id="${
+                good.id
+              }">
+                <span class="button-price">$${good.price}</span>
+              </button>
+             
+					</div>
+      `;
+      goodsContainer.append(goodBlock);
+
+      console.log(good);
+    });
   };
 
   const getData = (value, category) => {
@@ -25,10 +55,6 @@ const getGods = () => {
           // то есть если мы уже на странице с товарами, то тогда запускаем рендеринг и фильтрацию по категории или gender
         }
         renderGoods(array);
-
-        // window.location.href = "/goods.html"; //перевод на страницу с товарами
-
-        // console.log(window.location);
       });
   };
 

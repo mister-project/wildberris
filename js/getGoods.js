1ec8a1317a454825e9490e312f5aecef950c659b
Yutiy Kulikov <kulikov-y@yandex.ru>
2025-04-22 14:44:46 +0300

    работает фильтрации БД

--- js/getGoods.js
+++ js/getGoods.js
@@ -2,25 +2,28 @@ const getGods = () => {
   const links = document.querySelectorAll(".navigation-link");
   console.log(links);
 
-  const getData = () => {
-    fetch("https://willberiese-default-rtdb.firebaseio.com/db.json")
+  const getData = (value, category) => {
+    fetch("https://willberiese-default-rtdb.firebaseio.com/db.json") //подгрузка данных из удаленной БД
       .then((res) => res.json())
       .then((data) => {
-        console.log(data);
-        localStorage.setItem("data", JSON.stringify(data));
+        const array = category
+          ? data.filter((item) => item[category] === value)
+          : data;
+
+        localStorage.setItem("goods", JSON.stringify(array)); // загрузка массива из базы в localStorage
       });
   };
 
   links.forEach((link) => {
     link.addEventListener("click", (event) => {
-      event.preventDefault();
-      getData();
+      event.preventDefault(); //отменяем реакцию на кнопку по умолчанию - переход по ссылке
+      const linkValue = link.textContent;
+      const category = link.dataset.field;
+
+      console.log(category);
+
+      getData(linkValue, category);
     });
   });
-  const goods = JSON.parse(localStorage.getItem("goods"));
-
-  console.log(goods);
-  localStorage.removeItem("goods");
-  console.log(localStorage);
 };
 getGods();
